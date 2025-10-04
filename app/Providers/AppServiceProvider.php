c9241871c38bc6d652e5c2b25f385f0ac3e828d9
Dương Tùng Anh <tunnaduong@gmail.com>
2025-10-05 00:14:05 +0700

    Update AppServiceProvider.php

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -28,7 +28,7 @@ class AppServiceProvider extends ServiceProvider
   public function boot(): void
   {
     //
-    if (config('app.env') === 'production') {
+    if (config('app.env') != 'local') {
       URL::forceScheme('https');
       URL::forceRootUrl(config('app.url'));
     }
